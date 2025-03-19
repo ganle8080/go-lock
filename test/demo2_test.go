0cc508dd136360e4d1a4a1d0726a4412d9ea4900
subingjie <kf.subingjie@cloudnineinfo.com>
2025-03-19 09:24:22 +0800

    add

--- test/demo2_test.go
+++ test/demo2_test.go
@@ -1,17 +1,56 @@
 package test
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
 
 type gobalMapData struct {
 	gMutex sync.RWMutex
 	Name   string
-	maps   map[string]MapData
+	maps   map[string]*MapData
 }
 
 type MapData struct {
 	mMutex sync.RWMutex
 	Name   string
-	maps   map[string]string
+	maps   map[string]int
 }
 
-// 我将创建两个协程，一个在gobalMapData中修改数据，一个在MapData中修改数据
+var wg1 sync.WaitGroup
+
+func Add1(tm *gobalMapData) {
+
+	for i := 0; i < 5000; i++ {
+		tm.maps["mapdata"].mMutex.Lock()
+		tm.maps["mapdata"].maps["1"]++
+		tm.maps["mapdata"].mMutex.Unlock()
+	}
+	wg1.Done()
+}
+
+func TestXxx2(t *testing.T) {
+	gobal := gobalMapData{
+		Name: "gobal",
+	}
+
+	mapdata := MapData{
+		Name: "mapdata",
+	}
+
+	maps := map[string]int{
+		"1": 0,
+	}
+
+	mapdata.maps = maps
+	gobal.maps = make(map[string]*MapData)
+	gobal.maps["mapdata"] = &mapdata
+
+	wg1.Add(2)
+	go Add1(&gobal)
+	go Add1(&gobal)
+	wg1.Wait()
+
+	fmt.Printf("最终数据: %v\n", gobal.maps["mapdata"].maps["1"])
+}
